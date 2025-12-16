8ee915d5746fe2b74fa002f1bed8f52e5380d485
Dayrelis Mena Torres <dmentor@upo.es>
2025-12-16 18:33:28 +0100

    Hola master

--- src/epd9/Epd9.java
+++ src/epd9/Epd9.java
@@ -14,7 +14,7 @@ public class Epd9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola master");
         System.out.println("Desde develop");
     }
     
